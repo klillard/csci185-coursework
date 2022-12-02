1b6e9f16b1b2ebd531579593693a2f46aac7aeec
Katelyn <klillard@unca.edu>
2022-12-01 23:40:58 -0500

    homework 5 commit

--- homeworks/hw05/your_task/main.js
+++ homeworks/hw05/your_task/main.js
@@ -17,20 +17,94 @@ async function getTracks (term) {
     // send off the request to spotify and save the data in a variable
     // display the tracks in a nice way
 
-    let trackEndpoint = baseURL +"?";
-    trackEndpoint += "q=" + term + "&type=track"
-    console.log(trackEndpoint);
+    let tracksEndpoint = baseURL +"?";
+    tracksEndpoint += "q=" + term + "&type=track";
+    console.log(tracksEndpoint);
 
-    const artistData = await fetch(trackEndpoint).then(response => response.json());
-    console.log(artistData[0].name);
+    document.querySelector('#tracks').innerHTML = "";
 
-    document.querySelector('#track-section h1')
+    const trackData = await fetch(tracksEndpoint).then(response => response.json());
+    console.log(trackData);
+    console.log(trackData[0]);
+
+    // name of first track
+    console.log(trackData[0].name);
+
+    document.querySelector('#tracks').innerHTML = trackData[0].name;
+
+    let counter = 0;
+    while(counter < 5) {
+    const template = `
+    <section class="track-item preview" onclick ="playSong('${trackData[counter].id}')">
+                        <img src="${trackData[counter].album.image_url}">
+                        <i class="fas fa-play play-track" aria-hidden="true"></i>
+                        <div class="label">
+                            <h2>${trackData[counter].name}</h2>
+                            <p>
+                                ${trackData[counter].artist.name}
+                            </p>
+                        </div>
+                    </section>
+    `;
+
+    document.querySelector('#tracks').insertAdjacentHTML('beforeend', template);
+    counter++;
+    }
+    
 }
+
+function playSong(id) {
+    // let id = '5y8d8RHvxb0KiTa9Nq0xWp';
+    const template = `
+    <iframe style="border-radius:12px" 
+        src="https://open.spotify.com/embed/track/${id}?utm_source=generator" 
+        width="100%" 
+        height="352" 
+        frameBorder="0" 
+        allowfullscreen="" 
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+        loading="lazy"></iframe>
+        `;
+    document.querySelector('#artist').innerHTML = template;
+
+}
+
+
 async function getAlbums (term) {
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+
+    let albumEndpoint = baseURL +"?";
+    albumEndpoint += "q=" + term + "&type=album"
+    console.log(albumEndpoint);
+
+    const albumData = await fetch(albumEndpoint).then(response => response.json());
+    console.log(albumData[0].name);
+    console.log(albumData[0].image_url);
+
+    document.querySelector('#albums').innerHTML = "";
+
+    let counter = 0;
+    while(counter < 5) {
+    const template = `
+    <section id="albums">
+    <section class="album-card" id="${albumData[counter].id}">
+        <div>
+            <img src="${albumData[counter].image_url}">
+            <h2>${albumData[counter].name}</h2>
+            <div class="footer">
+                <a href="${albumData[counter].spotify_url}" target="_blank">
+                    view on spotify
+                </a>
+            </div>
+        </div>
+    </section>
+    </section>
+    `;
+
+    document.querySelector('#albums').insertAdjacentHTML('beforeend', template);
+    counter++;
+    }
+
+
 }
 
 async function getArtist (term) {
